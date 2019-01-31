c166f2a3f9504d2267a3030a4ea5602252bca6ec
anmolrajputit <anmol.singh_mca18@gla.ac.in>
2019-01-31 11:25:53 +0530

    Define Data Members

--- app/src/main/java/singh/anmol/uiviews/MainActivity.java
+++ app/src/main/java/singh/anmol/uiviews/MainActivity.java
@@ -2,8 +2,23 @@ package singh.anmol.uiviews;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Switch;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
+    private ImageButton imageButton;
+    private CheckBox checkBox;
+    private RadioGroup radioGroup;
+    private  RadioButton radioButton1, radioButton2;
+    private Switch aSwitch;
+    private ToggleButton toggleButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
